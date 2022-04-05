a88f10e8c5f69c3dda95ff00e811f0abd11f3cdd
Pavan-BN <77314903+Pavan-BN@users.noreply.github.com>
2022-04-06 02:11:07 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCs6Z7GIQEJJhVcstN1issDWB8bzr6dD10",
+    authDomain: "project-kwitter-f3e6d.firebaseapp.com",
+    projectId: "project-kwitter-f3e6d",
+    storageBucket: "project-kwitter-f3e6d.appspot.com",
+    messagingSenderId: "398118197239",
+    appId: "1:398118197239:web:10047394acf69968c480f2"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
